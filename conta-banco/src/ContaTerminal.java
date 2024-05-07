9e166a530df50af2cb72ad5458fdb7ba913647ad
Acassiodev <acassio_15_@hotmail.com>
2024-05-07 10:15:18 -0300

    Atv pronta

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,85 @@
-public class ContaTerminal{
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World alo alo");
+import java.util.Scanner;
+
+public class ContaTerminal {
+    private int numero;
+    private String agencia;
+    private String nomeCliente;
+    private double saldo;
+
+    // Método construtor
+    public ContaTerminal(int numero, String agencia, String nomeCliente, double saldo) {
+        this.numero = numero;
+        this.agencia = agencia;
+        this.nomeCliente = nomeCliente;
+        this.saldo = saldo;
+    }
+
+    // Método para exibir os detalhes da conta
+    public void exibirDetalhes() {
+        System.out.println("Número da Conta: " + numero);
+        System.out.println("Agência: " + agencia);
+        System.out.println("Nome do Cliente: " + nomeCliente);
+        System.out.println("Saldo: " + saldo);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o número da conta:");
+        int numeroConta = scanner.nextInt();
+        scanner.nextLine(); // Consumir a quebra de linha pendente
+
+        System.out.println("Por favor, digite o número da Agência !");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Digite o nome do cliente:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Digite o saldo:");
+        double saldo = scanner.nextDouble();
+
+        ContaTerminal conta = new ContaTerminal(numeroConta, agencia, nomeCliente, saldo);
+        conta.exibirDetalhes();
+
+        // Exibir mensagem personalizada
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +
+                ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        scanner.close();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       //TODO:Conhecer e importar a classe Scanner
+
+       //Exibir as mensagens para o nosso usuario
+
+       //Obter pela scanner os valores digitados no terminal 
+
+       //Exibir a mensagem da conta criada 
+    
